f32e36de1b96bc2ea0b32e98c0df3b60577b90bb
nhsdis <107676487+nhsdis@users.noreply.github.com>
2022-07-21 08:07:15 +0200

    Add files via upload

---
+++ informations.js
@@ -0,0 +1,16 @@
+var infosCervicales = {
+  question1  : "L’écran principal n’étant pas placé en face de vous (ou vos écrans n’étant pas côte à côte et placer en face de vous) génère des torsions cervicales répétées ou prolongées. Préférez le placement de votre écran principal face à vous afin de conserver un bon alignement de vos vertèbres cervicales.",
+  question2_sans_verres_pro : "L’écran placé trop bas ou trop haut peut générer d’importantes contraintes de flexions ou d’extensions sur vos cervicales. Un réglage avec votre écran placé à la hauteur de vos yeux est à privilégier afin de conserver un bon alignement de vos vertèbres cervicale.",
+  question2_verres_pro  : "L’écran placé trop bas, trop haut ou insuffisamment incliné peut générer d’importantes contraintes sur vos cervicales. Un réglage avec votre écran placé en position basse et incliné vers l’avant est à privilégier afin de conserver un bon alignement de vos vertèbres cervicales."
+};
+
+var infosYeux = {
+  question1  : "Une fenêtre placée derrière votre écran (problèmes de reflets) ou face à votre écran (problèmes d’éblouissements) peut contribuer à une fatigue visuelle accélérée. Privilégier un positionnement perpendiculaire de votre écran par rapport aux fenêtres situées à proximité.",
+  question2 : "La lumière bleue artificielle qui est générée par les écrans peut anticiper la fatigue visuelle et perturber vos nuits par des troubles du sommeil. En réglant les paramétrages de votre écran, il vous est possible de réduire la lumière bleue en accentuant les couleurs chaudes. Ce paramétrage peut être mis en place à partir d’une certaine heure de la journée. Cliquer sur « Démarrer », « Paramètres », « Système », « Affichage », « Paramètres d’éclairage nocturne »."
+};
+
+var infosEpaules = {
+  question1  : "Les avant-bras qui ne peuvent reposer sur un support peuvent provoquer d’importantes contraintes sur vos épaules et avoir tendance à vouter votre corps vers l’avant. Privilégier le repos de vos avant-bras sur les accoudoirs ou le bureau pour soulager le haut de votre corps.",
+  question2 : "Une souris trop éloignée de vous ou de votre clavier peut engendrer une importante sollicitation de votre épaule. Préférez une utilisation de la souris proche de vous ou du clavier afin de limiter ces contraintes.",
+  question3  : "Un manque de place entre votre clavier et le rebord de votre bureau peut vous contraindre dans le placement de vos poignets et avant-bras et inciter le haut de votre corps à se vouter vers l’avant. Veiller à conserver un espace suffisant (10cm minimum) entre le clavier et le rebord du bureau pour une meilleure installation."
+};
